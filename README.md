8cde07b1b91f06edfa6abeb24fd74e2573dc94e5
Ben <mrbenruggles@gmail.com>
2023-07-03 21:21:54 +0200

    forge prac initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Learning Foundry/Anvil
+
+Following PatrickAlphaC's tutorial on Foundry/Anvil in order to learn this framework.
+Very Similar to his other guides however this framework solely uses Solidity which I am very enticed by.
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+// 1. Deploy mocks when on local Anvil chain
+// 2. Keep track of contract addr accross different chains
+// Sepolia ETH/USD
+// Mainnet ETH/USD
+
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    // Deploy mocks if on anvil
+    // Otherwise grab addr from existing network
+    NetworkConfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+    uint256 public constant SEPOLIA_CHAIN_ID = 11155111;
+    uint256 public constant MAINNET_CHAIN_ID = 1;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed addr
+    }
+
+    constructor() {
+        if (block.chainid == SEPOLIA_CHAIN_ID) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == MAINNET_CHAIN_ID) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory mainnetConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return mainnetConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        // Deploy mocks & return mock addr
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
+    }
+}
